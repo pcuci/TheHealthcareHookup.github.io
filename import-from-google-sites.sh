669e09cfd6801319990e0d0f5a65b10ddce1e0bf
Paul Cuciureanu <paulcu@gmail.com>
2017-01-31 22:05:08 -0500

    Adding redirect

--- import-from-google-sites.sh
+++ import-from-google-sites.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget -P web --timestamping --convert-links --page-requisites --no-directories --no-host-directories -erobots=off -l1 --span-hosts --adjust-extension https://sites.google.com/view/thehealthcarehookup
+
+mv web/thehealthcarehookup.html web/index.html
